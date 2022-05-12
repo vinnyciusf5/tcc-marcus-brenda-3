36c7a36eb1c0bccbed0c9e3c88e0ccc5e8f14577
Marcus V. B. Oliveira <vinnyciusf5@hotmail.com>
2022-05-12 00:49:26 -0300

    main

---
+++ cadastro/config.php
@@ -0,0 +1,14 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario'
+
+    $conexao = new mysqli( $dbHost,$dbUsername,$dbPassword,$dbName);
+
+    if($conexao->connect_errno){
+        echo "Erro";
+    }else{
+        echo "conexõ conectada com sucesso";
+    }
+?>
